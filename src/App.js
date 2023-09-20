ecb52dfdc3fc46ddc401c11b7f8c8c65f98bc638
UdumiziSolomon <solomonudumizi@gmail.com>
2023-09-20 09:37:42 +0100

    feat: router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
+import { Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
+        <Routes>
+          {/* <Route name="Home" /> */}
+          {/* <Route name="Contact" /> */}
+          {/* <Route name="Register" /> */}
+        </Routes>
+
       <h1 style={{ color: '#fff', fontFamily: "Bold" }}>GET LINKED</h1>
       <h1 style={{ color: '#fff', fontFamily: "SemiBold" }}>GET LINKED</h1>
       <h1 style={{ color: '#fff', fontFamily: "Medium" }}>GET LINKED</h1>
