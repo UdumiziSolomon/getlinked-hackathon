d14db9de10bb75415d7abc6951b76b3a190e2d8f
UdumiziSolomon <solomonudumizi@gmail.com>
2023-09-20 12:55:24 +0100

    feat: montserrat

--- src/pages/home/sections/timeline/Timeline.jsx
+++ src/pages/home/sections/timeline/Timeline.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import styles from './Timeline.module.css';
+import styles from "./Timeline.module.css";
 
 const Timeline = () => {
   return (
     <div className={styles.container}>
-      <h1>Timeline</h1>
+      <div className={styles.topLayer}>
+        <h1 className={styles.topLayerTopText}> Timeline </h1>
+        <h2 className={styles.topLayerSubText}>
+          Here is the breakdown of the time we anticipate using for the upcoming
+          event.
+        </h2>
+      </div>
     </div>
   );
 };
