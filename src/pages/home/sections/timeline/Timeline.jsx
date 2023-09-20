dde020368364884d1d93f90b14a3996857d4e33f
UdumiziSolomon <solomonudumizi@gmail.com>
2023-09-20 10:40:54 +0100

    feat: timeLine stx

---
+++ src/pages/home/sections/timeline/Timeline.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Timeline.module.css';
+
+const TimeLine = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Timeline</h1>
+    </div>
+  );
+};
+
+export default TimeLine;
