1321b9edce7954d80a11b4bb4bbfee36077dd670
UdumiziSolomon <solomonudumizi@gmail.com>
2023-09-20 09:48:28 +0100

    feat: added react query config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
-import { Routes } from 'react-router-dom';
+import { Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// new queryClient
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div className="App">
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
         <Routes>
           {/* <Route name="Home" /> */}
           {/* <Route name="Contact" /> */}
           {/* <Route name="Register" /> */}
         </Routes>
 
-      <h1 style={{ color: '#fff', fontFamily: "Bold" }}>GET LINKED</h1>
-      <h1 style={{ color: '#fff', fontFamily: "SemiBold" }}>GET LINKED</h1>
-      <h1 style={{ color: '#fff', fontFamily: "Medium" }}>GET LINKED</h1>
-      <h1 style={{ color: '#fff', fontFamily: "Regular" }}>GET LINKED</h1>
-      <h1 style={{ color: '#fff', fontFamily: "Light" }}>GET LINKED</h1>
-      <h1 style={{ color: '#fff', fontFamily: "ExtraLight" }}>GET LINKED</h1>
-    </div>
+        <h1 style={{ color: "#fff", fontFamily: "Bold" }}>GET LINKED</h1>
+        <h1 style={{ color: "#fff", fontFamily: "SemiBold" }}>GET LINKED</h1>
+        <h1 style={{ color: "#fff", fontFamily: "Medium" }}>GET LINKED</h1>
+        <h1 style={{ color: "#fff", fontFamily: "Regular" }}>GET LINKED</h1>
+        <h1 style={{ color: "#fff", fontFamily: "Light" }}>GET LINKED</h1>
+        <h1 style={{ color: "#fff", fontFamily: "ExtraLight" }}>GET LINKED</h1>
+      </div>
+    </QueryClientProvider>
   );
 };
 
