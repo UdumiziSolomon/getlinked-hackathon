a8876ac7f26050ff7894d70f6ddda8948a25f361
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-09-22 03:04:38 +0100

    nggfj

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <main className='contact'>
+        Contact
+    </main>
   )
 }
 
